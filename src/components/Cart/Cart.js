bbedc1ccefc483c197fafec5943ba86cad21fd19
Neeraj <nxxwxj@gmail.com>
2022-01-28 17:18:35 +0530

    added a Modal via React Portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,17 @@
+import Modal from '../UI/Modal'
 import classes from './Cart.module.css'
 
 const Cart = props => {
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {' '}
-      {[{ id: 'c1', name: 'Sus', amount: 2, price: 15 }].map(item => {
-        ;<li>{item.name}</li>
-      })}{' '}
+      {[{ id: 'c1', name: 'Sus', amount: 2, price: 15 }].map(item => (
+        <li>{item.name}</li>
+      ))}
     </ul>
   )
 
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total</span>
@@ -21,7 +21,7 @@ const Cart = props => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
