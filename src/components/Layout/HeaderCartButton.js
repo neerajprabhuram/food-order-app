4a7965dcae77b892ee31be8fd55c2f87f1b1e457
Neeraj <nxxwxj@gmail.com>
2022-01-30 21:47:51 +0530

    added amination to HeaderCartButton using useEffect and useState

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,40 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = props => {
-  const cartCtx = useContext(CartContext)
+  const [buttonHighlight, setButtonHighlight] = useState(false)
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item)=>{
+  const cartCtx = useContext(CartContext)
+  const {items} = cartCtx
+
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount
-  },0)
+  }, 0)
+
+
+  const btnClasses = `${classes.button} ${buttonHighlight ? classes.bump : ''}`
+
+  useEffect(() => {
+    if(items.length === 0) {
+      return
+    }
+    
+    setButtonHighlight(true)
+
+    const timer = setTimeout(()=>{
+      setButtonHighlight(false)
+    },300)
+
+    return ()=>{
+      clearTimeout(timer)
+    }
+  }, [items])
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
